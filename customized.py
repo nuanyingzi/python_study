635eaff703acd0d6b7f54e5682558c3e766c3c80
tao <852947475@qq.com>
2023-06-12 17:59:37 +0800

    类与对象

--- customized.py
+++ customized.py
@@ -8,7 +8,25 @@ class Student(object):
         return 'Student object (name: %s)' % self.name
     __repr__ = __str__
 
+
 s = Student('Micahel')
 print(s)
-s
+
+
+class Fib(object):
+    def __init__(self):
+        self.a, self.b = 0, 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        if self.a > 100000:
+            raise StopIteration()
+        return self.a
+
+
+for n in Fib():
+    print(n)
 
